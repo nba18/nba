5ce25be0fea927159c0a1831b4576b6a7761ee08
Nguyen Binh An <nba18012001@gmail.com>
2022-03-03 15:09:39 +0700

    cai dat cac handler

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req ,res,next) => {
-    ErrorHandler.handleError(error, res);
+    ErrorHandler.handleError(err, res);
 });
 module.exports = app;
--- app/config/index.js
+++ app/config/index.js
@@ -2,6 +2,10 @@ const config = {
     app: {
         port: process.env.port || 8070,
     },
+
+    db: {
+        uri: process.env.MONGODB_URI || "mongodb+srv://nba18:binhan2001@cluster0.6lfn3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    }
 };
 
 module.exports = config;
